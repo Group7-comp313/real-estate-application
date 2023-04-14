1dfe37acfea2f0d37f788673914fa2bdef8689d5
0822mahesh <72593545+0822mahesh@users.noreply.github.com>
2023-04-14 19:56:52 -0400

    Update nginx.sh

--- nginx.sh
+++ nginx.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-sudo cp -rf app.conf /etc/nginx/sites-available/web_site
+sudo cp -rf app.conf /etc/nginx/conf.d
 chmod 710 /var/lib/jenkins/workspace/django-cicd
 
-sudo ln -s /etc/nginx/sites-available/web_site /etc/nginx/sites-enabled
+
 sudo nginx -t
 
 sudo systemctl start nginx
