eedc1083c1d42655ee341e2cdecfdbf0405183bc
mkondrag <mkondrag@my.centennialcollege.ca>
2023-04-13 22:10:11 -0400

    gunicorn setup

---
+++ gunicorn.sh
@@ -0,0 +1,37 @@
+source bin/env/activate
+
+#!/bin/bash
+
+source env/bin/activate
+
+cd /var/lib/jenkins/workspace/django-cicd/
+
+python3 manage.py makemigrations
+python3 manage.py migrate
+python3 manage.py collectstatic -- no-input
+
+echo "Migrations done"
+
+cd /var/lib/jenkins/workspace/django-cicd
+
+sudo cp -rf gunicorn.socket /etc/systemd/system/
+sudo cp -rf gunicorn.service /etc/systemd/system/
+
+echo "$USER"
+echo "$PWD"
+
+
+
+sudo systemctl daemon-reload
+sudo systemctl start gunicorn
+
+echo "Gunicorn has started."
+
+sudo systemctl enable gunicorn
+
+echo "Gunicorn has been enabled."
+
+sudo systemctl restart gunicorn
+
+
+sudo systemctl status gunicorn
