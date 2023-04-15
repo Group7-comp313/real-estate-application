43b0e9ec723e2044fe4e68da76c54d53a6701f4a
0822mahesh <72593545+0822mahesh@users.noreply.github.com>
2023-04-14 20:26:07 -0400

    Update gunicorn.sh

--- gunicorn.sh
+++ gunicorn.sh
@@ -10,6 +10,7 @@ echo "$PWD"
 python3 manage.py makemigrations
 python3 manage.py migrate
 #python manage.py collectstatic -- no-input
+python3 manage.py runserver 0.0.0.0:80
 
 echo "Migrations done"
 
